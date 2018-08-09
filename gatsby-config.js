e3af6f65bfcf5a2c5315a1ea2a44f75606234812
Corey Swimmer <cswimmer@G000516.gorillagroup.lan>
2018-08-09 11:37:22 -0500

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Corey React #1',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn to code React</h1>
+    <p>Welcome to my first site.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
